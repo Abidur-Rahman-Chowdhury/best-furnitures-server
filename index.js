7645353acbd8047eaa05afe4257afedbd2707bf5
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-29 19:43:50 +0600

    connect with mongodb and implement get api to get data from database

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ app.get('/', (req, res) => {
     res.send('Inventory Server is running');
 
 })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@furniture-inventory-man.tnveo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const furnitureCollection = client.db("inventory").collection("furnitures");
+        app.get('/furnitures', async (req, res) => {
+            const query = {};
+            const cursor = furnitureCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result)
+            
+        })
 
+
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
 
 
 
