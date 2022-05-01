384291b091402ec151ca86c4e52af818e336c84e
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-02 03:42:36 +0600

    delete from manage inventory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { config } = require('dotenv');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,13 +21,26 @@ async function run() {
     try {
         await client.connect();
         const furnitureCollection = client.db("inventory").collection("furnitures");
+
+        // GET: get all furniture and send back to client
         app.get('/furnitures', async (req, res) => {
             const query = {};
             const cursor = furnitureCollection.find({});
             const result = await cursor.toArray();
             res.send(result)
             
+        });
+
+        // DELETE: delete item from manage Inventories 
+        app.delete('/furnitures/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await furnitureCollection.deleteOne(query);
+            res.send(result);
+            
         })
+
 
 
     } finally {
